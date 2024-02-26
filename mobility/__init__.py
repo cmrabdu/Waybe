23fadbe57ee0dfd228d29167ebd16352e59d778e
Noah Moussaoui <noah.moussaoui@student.uclouvain.be>
2024-02-26 15:20:49 +0000

    Update __init__.py

--- mobility/__init__.py
+++ mobility/__init__.py
@@ -27,24 +27,31 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-@app.route('/base')
-def base():
-    return render_template('base.html')
+    @app.route('/base')
+    def base():
+        return render_template('base.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html', HelloWorld='Hello World')
+    @app.route('/about')
+    def about():
+        return render_template('about.html', HelloWorld='Hello World')
 
-@app.route('/noah')
-def noah():
-    return render_template('noah.html',noah='hello world')
+    @app.route('/noah')
+    def noah():
+        return render_template('noah.html',noah='hello world')
 
-@app.route('/abdullah')
-def abdullah():
-    return render_template('abdullah.html',abdullah='hello world')
+    @app.route('/abdullah')
+    def abdullah():
+        return render_template('abdullah.html',abdullah='hello world')
 
-if __name__ == '__main__':
-    URL = 'http://127.0.0.1:5000/base'
-    webbrowser.open_new_tab(URL)
-    app.run(debug=True)
+    
+    @app.route('/abdullah')
+    def houari():
+        return render_template('houari.html',houari='hello world')
+
+
+    @app.route('/abdullah')
+    def guillaume():
+        return render_template('guillaume.html',guillaume='hello world')
+
+    return app 
 
