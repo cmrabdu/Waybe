604ddfe6626cafad43ddabddd9e3fea691a87b8d
Noah Moussaoui <noah.moussaoui@student.uclouvain.be>
2024-03-07 16:01:44 +0000

    Update __init__.py

--- mobility/__init__.py
+++ mobility/__init__.py
@@ -1,9 +1,18 @@
+import os
+from flask import Flask, render_template
+import webbrowser
+
+app = Flask(__name__)
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-    )
+    if test_config:
+        app.config.from_mapping(test_config)
+    else:
+        app.config.from_mapping(
+            SECRET_KEY='dev',
+            DATABASE=os.path.join(app.root_path, 'poudlard.sqlite'),
+        )
 
     from . import city
     app.register_blueprint(city.bp)
@@ -22,6 +31,9 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    
+
 
     @app.route('/')
     def base():
