413a2096a2454250ac47e2e7e7d55e0f7b635373
Noah <noah.moussaoui@student.uclouvain.be>
2024-03-07 03:29:05 +0100

    mise en place de base

--- mobility/__init__.py
+++ mobility/__init__.py
@@ -1,61 +1,44 @@
-import os
 from flask import Flask, render_template
-import webbrowser
+import os
+
 app = Flask(__name__)
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    if test_config:
-        app.config.from_mapping(test_config)
-    else:
-        app.config.from_mapping(
-            SECRET_KEY='dev',
-            DATABASE=os.path.join(app.root_path, 'poudlard.sqlite'),
-        )
 
-    from . import city
-    app.register_blueprint(city.bp)
+# Configuration
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.root_path, 'poudlard.sqlite'),
+)
 
-    app.add_url_rule('/', endpoint='index')
+# Ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+# Routes
+@app.route('/')
+def base():
+    return render_template('base.html')
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+@app.route('/about')
+def about():
+    return render_template('about.html', HelloWorld='Hello World')
 
-    @app.route('/base')
-    def base():
-        return render_template('base.html')
+@app.route('/noah')
+def noah():
+    return render_template('noah.html', noah='hello world')
 
-    @app.route('/about')
-    def about():
-        return render_template('about.html', HelloWorld='Hello World')
+@app.route('/abdullah')
+def abdullah():
+    return render_template('abdullah.html', abdullah='hello world')
 
-    @app.route('/noah')
-    def noah():
-        return render_template('noah.html',noah='hello world')
+@app.route('/houari')
+def houari():
+    return render_template('houari.html', houari='hello world')
 
-    @app.route('/abdullah')
-    def abdullah():
-        return render_template('abdullah.html',abdullah='hello world')
+@app.route('/guillaume')
+def guillaume():
+    return render_template('guillaume.html', guillaume='hello world')
 
-    
-    @app.route('/houari')
-    def houari():
-        return render_template('houari.html',houari='hello world')
-
-
-    @app.route('/guillaume')
-    def guillaume():
-        return render_template('guillaume.html',guillaume='hello world')
-
-    return app 
-
+if __name__ == "__main__":
+    app.run(debug=True)
