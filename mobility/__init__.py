26aa9b6bcff286c9176e22d05bf07a44d003e116
Noah Moussaoui <noah.moussaoui@student.uclouvain.be>
2024-03-11 10:22:26 +0000

    Update __init__.py pour le test de merge

--- mobility/__init__.py
+++ mobility/__init__.py
@@ -1,59 +1,54 @@
-from flask import Flask, render_template
 import os
-import sqlite3
+from flask import Flask, render_template
+import webbrowser
 
 app = Flask(__name__)
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    if test_config:
+        app.config.from_mapping(test_config)
+    else:
+        app.config.from_mapping(
+            SECRET_KEY='dev',
+            DATABASE=os.path.join(app.root_path, 'poudlard.sqlite'),
+        )
 
-# Configuration
-app.config.from_mapping(
-    SECRET_KEY='dev',
-    DATABASE=os.path.join(app.root_path, 'poudlard.sqlite'),
-)
+    from . import city
+    app.register_blueprint(city.bp)
 
-# Ensure the instance folder exists
-try:
-    os.makedirs(app.instance_path)
-except OSError:
-    pass
+    app.add_url_rule('/', endpoint='index')
 
-def obtenir_stats_villes():
-    # Connexion à la base de données SQLite
-    connexion = sqlite3.connect('test2.db')
-    db = connexion.cursor()
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
 
-    # Exécutez une requête pour sélectionner toutes les villes
-    db.execute("SELECT COUNT(*) FROM ville;")
-    nombre_de_villes = db.fetchone()[0]
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-    # Exécutez une autre requête pour obtenir les noms de toutes les villes
-    db.execute("SELECT nom FROM ville;")
-    noms_des_villes = db.fetchall()
-
-    # Fermer la connexion à la base de données
-    connexion.close()
-
-    # Retourne les données récupérées
-    return nombre_de_villes, noms_des_villes
-# Routes
-@app.route('/')
-def base():
-    return render_template('base.html')
+    
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html', HelloWorld='Hello World')
+    @app.route('/')
+    def base():
+        return render_template('base.html',base='bonjour')
 
+    @app.route('/about')
+    def about():
+        return render_template('about.html', about='Bonjour tout le monde')
 
-@app.route('/stats')
-def stats():
-    return render_template('stats.html', stats='hello world')
+    @app.route('/statistique')
+    def stats():
+        return render_template('stats.html', stats='bonjour le monde')
 
+    @app.route('/request')
+    def request():
+        return render_template('request.html', data='bonjour le monde')
 
-@app.route('/request')
-def request():
-    return render_template('request.html', abdullah='hello world')
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return app
